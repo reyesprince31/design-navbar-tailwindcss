b6f0e6ffd78e3ad468ec9551340830ecc8b138c7
Prince Reyes <krayzerprince@gmail.com>
2023-11-03 21:57:56 +0800

    make the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-gray-900">
-      <div className="flex items-center justify-between px-4 py-3">
+    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+      <div className="flex items-center justify-between px-4 py-3 sm:p-0">
         <div>
           <img className="h-8" src={logoInverted} alt="Workcation" />
         </div>
-        <div>
+        <div className="sm:hidden">
           <button
             type="button"
             onClick={() => setIsOpen(!isOpen)}
@@ -31,20 +31,23 @@ function Navbar() {
           </button>
         </div>
       </div>
-      <div className={`px-2 pt-2 pb-4 ${isOpen ? "block" : "hidden"}`}>
+      <div
+        className={`px-2 pt-2 pb-4 sm:flex sm:p-0 ${
+          isOpen ? "block" : "hidden"
+        }`}>
         <a
           href="#"
-          className="block text-white font-semibold hover:bg-gray-800 px-2 py-1 mt-1">
+          className="block text-white font-semibold hover:bg-gray-800 px-2 py-1 mt-1 sm:ml-2">
           List your property
         </a>
         <a
           href="#"
-          className="block text-white font-semibold hover:bg-gray-800 px-2 py-1 mt-1">
+          className="block text-white font-semibold hover:bg-gray-800 px-2 py-1 mt-1 sm:ml-2">
           Trips
         </a>
         <a
           href="#"
-          className="block text-white font-semibold hover:bg-gray-800 px-2 py-1 mt-1">
+          className="block text-white font-semibold hover:bg-gray-800 px-2 py-1 mt-1 sm:ml-2">
           Messages
         </a>
       </div>
