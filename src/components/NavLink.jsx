bdbc8170a1232351d1860e330a5f9518677c0a01
Prince Reyes <krayzerprince@gmail.com>
2023-11-04 21:48:13 +0800

    fix hover bg issue

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,9 +1,10 @@
 function NavLink({ text, sticky }) {
-  const hoverColor = sticky ? "gray-300" : "gray-800";
   return (
     <a
       href="#"
-      className={`block font-semibold px-2 py-1 mt-1 sm:ml-2 hover:bg-${hoverColor}`}>
+      className={`block font-semibold ${
+        sticky ? "hover:bg-gray-300" : "hover:bg-gray-800"
+      } px-2 py-1 mt-1 sm:ml-2`}>
       {text}
     </a>
   );
